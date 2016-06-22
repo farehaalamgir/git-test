da1ca443c68f4b2769e3397f02219b4f229fabf5
farehaalamgir <farehakhanalamgir@gmail.com>
2016-06-22 22:50:16 +0600

    added puti

--- src/friendsPackage/Test.java
+++ src/friendsPackage/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		String name = "This is a test ,i love java";
-		String namwTwo = "Sam loves huz";
+		String namwTwo = "Puti Mach Picchi chele :P hehe";
 		System.out.println(namwTwo);
 		System.out.println(name);
 
