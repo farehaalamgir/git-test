1a48b220b5a3bf3c917efb07d8f04e342d715b14
Fareha Alamgir <farehakhanalamgir@gmail.com>
2016-06-22 22:15:17 +0600

    Sam huz added

--- src/friendsPackage/Test.java
+++ src/friendsPackage/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		String name = "This is a test ,i love java";
+		String namwTwo = "Sam loves huz";
+		System.out.println(namwTwo);
 		System.out.println(name);
 
 	}
